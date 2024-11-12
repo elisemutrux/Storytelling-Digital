ba196ca70a8f1a5aa46fa5db6a5ceb96c2322b91
elisemutrux <122873450+elisemutrux@users.noreply.github.com>
2024-11-12 12:05:08 +0100

    btn-skip + début slider

--- src/js/main.js
+++ src/js/main.js
@@ -6,8 +6,10 @@ gsap.registerPlugin(ScrollTrigger);
 window.addEventListener("DOMContentLoaded", function () {
   var startupScreenBtn = document.querySelector(".startup-btn");
   var startupScreen = document.querySelector(".startup-screen");
+  var gameScreen = document.querySelector(".game");
 
   startupScreenBtn.addEventListener("click", () => {
     startupScreen.classList.add("is-press");
+    gameScreen.classList.add("is-press");
   });
 });
