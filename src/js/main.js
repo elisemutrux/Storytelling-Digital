95977a48e1faee7b78efd7ffd24b826cdbb22f7e
elisemutrux <122873450+elisemutrux@users.noreply.github.com>
2024-11-13 11:46:43 +0100

    scroll bloquer

--- src/js/main.js
+++ src/js/main.js
@@ -8,11 +8,15 @@ window.addEventListener("DOMContentLoaded", function () {
   var startupScreen = document.querySelector(".startup-screen");
   var gameScreen = document.querySelector(".game");
   var map = document.querySelector(".carte-img");
+
+  var containerImage1 = document.querySelector(".vignette-container-1");
+
   var firstImageBox = document.querySelector(".vignette-box.t-1");
   var firstImageBtn1 = document.querySelector(".vignette-question.t-1");
   var firstImageBtn2 = document.querySelector(
     ".vignette-question.t-second.t-1"
   );
+
   var second1ImageBox = document.querySelector(".vignette-box.t-2-1");
   var second2ImageBox = document.querySelector(".vignette-box.t-2-2");
 
@@ -44,14 +48,12 @@ window.addEventListener("DOMContentLoaded", function () {
       "<"
     );
 
-    tl.to(firstImageBox, {
+    tl.to(containerImage1, {
       display: "block",
 
-      // onComplete: () => {
-      //   if (isSkipped == false) {
-      //     document.body.style.overflow = "hidden";
-      //   }
-      // },
+      onComplete: () => {
+        document.body.style.overflow = "hidden";
+      },
     });
 
     firstImageBtn1.addEventListener("click", () => {
